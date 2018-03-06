6fb970913d3a1a5fdc6441d86dd0329218325139
Charlene Bastians <charlenebastians@hotmail.com>
2018-03-06 17:01:06 +0000

    refactored with labels

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -4,6 +4,7 @@ feature 'Testing infrastructure' do
     fill_in 'name1', with: 'Charlene'
     fill_in 'name2', with: 'Cindy'
     click_button 'submit'
+    # This doesn't explicitly check if redirects to /names
     expect(page).to have_content 'Charlene VS Cindy'
   end
 end
