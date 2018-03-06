a9362b64f584de761a0b974e1903503e3a4a41e8
Charlene Bastians <charlenebastians@hotmail.com>
2018-03-06 18:08:01 +0000

    players can now attack

--- app.rb
+++ app.rb
@@ -20,5 +20,16 @@ class Battle < Sinatra::Base
     erb :play
   end
 
+  get '/play' do
+    erb :attack
+  end
+
+
+  get '/attack' do
+    @name1 = session[:name1]
+    @name2 = session[:name2]
+    erb :attack
+  end
+
   run! if app_file == $0
 end
