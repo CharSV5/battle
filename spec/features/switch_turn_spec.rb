4a9a138f0f20cebadf8dfff9829c6518cd000452
Charlene Bastians <charlenebastians@hotmail.com>
2018-03-08 12:09:29 +0000

    switch turns, all tests passing

--- spec/features/switch_turn_spec.rb
+++ spec/features/switch_turn_spec.rb
@@ -2,13 +2,12 @@ feature'#switch_turn' do
   context 'seeing the current turn' do
     scenario 'start of the game' do
       sign_in_and_play
-      click_link 'Attack'
       expect(page).to have_content "Charlene's turn"
     end
     scenario ' after player 1 attacks player 2' do
       sign_in_and_play
       click_link 'Attack'
-      click_link 'Attack'
+      click_link 'OK'
       expect(page).not_to have_content "Charlene's turn"
       expect(page).to have_content "Cindy's turn"
     end
